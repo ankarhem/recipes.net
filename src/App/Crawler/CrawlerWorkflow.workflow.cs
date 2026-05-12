47abe47fdce329a38c1a33651d319be291bbabb4
Jakob Ankarhem <jakob@ankarhem.dev>
2026-05-12 23:40:55 +0200

    Remove non-deterministic workflow delay from crawl loop

--- src/App/Crawler/CrawlerWorkflow.workflow.cs
+++ src/App/Crawler/CrawlerWorkflow.workflow.cs
@@ -18,7 +18,6 @@ public class CrawlerWorkflow
         while (_urlQueue.Count > 0)
         {
             var url = _urlQueue.Dequeue();
-            await Workflow.DelayAsync(TimeSpan.FromMilliseconds(new Random().Next(300, 1000)));
             await HandlePageAsync(url);
         }
     }
